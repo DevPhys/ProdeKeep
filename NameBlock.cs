70fdb399c310cdea43be30c94ebdc746d8dd1280
VladTechDev <github.csv@gmail.com>
2026-08-26 09:58:53 +0300

    Add files via upload

---
+++ NameBlock.cs
@@ -0,0 +1,27 @@
+using Godot;
+using System.Collections.Generic;
+using System;
+using System.Collections.Concurrent;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public partial class NameBlock : Label
+{
+	int currentBlock;
+	Dictionary<int, string> nameBlocks = Storage.NameBlocks;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		currentBlock = IventoryNode.currentBlock;
+		Text = $"{nameBlocks[currentBlock]}";
+	}
+}
